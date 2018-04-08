80e0091217bf64d37277ec900094ed5fc84c649a
tlnob <thais.nobre7@gmail.com>
2018-04-08 01:20:13 -0300

    relações entre entidades

--- api.py
+++ api.py
@@ -1,7 +1,5 @@
 import glob, os, re, json
 
-
-
 data = '/home/thais/Desktop/HackatonEMTU/gtfs_emtu/'
 
 def unquote(str):
@@ -26,13 +24,38 @@ def parse(fileLines):
 			arr.append(obj)
 	return arr
 
+def primarykey(db, table, key):
+	arr = db[table]
+	out = {}
+	for obj in arr:
+		out[obj[key]] = obj
+	db[table] = out
+
+def trip_of(db, table, key):
+	for trip_id in db['trips'].keys():
+		trip = db["trips"][trip_id]
+		obj_id = trip[key]
+		obj = db[table][obj_id]
+		obj.setdefault("trips", [])
+		obj["trips"].append(trip_id)
+
+
 db = {}
 for datafile in glob.glob(os.path.join(data, '*.txt')):
 	file = open(datafile, "r")
 	fileLines = file.readlines()
 	table = datafile.replace(data,'')[:-4]
 	db[table] = parse(fileLines)
+#	json.dumps(db, sort_keys=True, indent=4)
 
-print(json.dumps(db, sort_keys=True, indent=4))
+primarykey(db, "trips", "trip_id")
+primarykey(db, "shapes", "shape_id")
+primarykey(db, "routes", "route_id")
+
+trip_of(db, "shapes", "shape_id")
+trip_of(db, "routes", "route_id")
+
+print(json.dumps(db['routes'], sort_keys=True, indent=4))
+
 
 
